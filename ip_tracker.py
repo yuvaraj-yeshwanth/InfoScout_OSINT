ae31654fdb9e713e10fc83921fe14eb484181da5
Yuvaraj <yuvarajyeshwanth7574@gmail.com>
2024-12-15 21:23:00 +0530

    Add files via upload

---
+++ ip_tracker.py
@@ -0,0 +1,6 @@
+
+def track_ip():
+    ip = input("Enter IP address: ")
+    print(f"Tracking details for IP: {ip}")
+    # Dummy data for example
+    print("Location: Chennai, India\nISP: Example ISP\nOrganization: Example Org")
